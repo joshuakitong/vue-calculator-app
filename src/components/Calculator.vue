e9f0b71e63769a52d9683f652e2775d4ba649c22
joshuakitong <joshuakitong@gmail.com>
2025-06-29 22:41:29 +0800

    fix: minor design adjustments

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -124,7 +124,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="min-h-screen flex items-center justify-center bg-gray-300 px-4">
+  <div class="min-h-screen flex items-center justify-center bg-gray-200 px-4">
     <div class="w-full max-w-md bg-gray-600 rounded-xl shadow-xl p-4">
       <h1 class="text-center text-2xl font-bold mb-4 text-white">{{ title }}</h1>
       <Display :expression="expression" :result="result" />
--- src/components/Display.vue
+++ src/components/Display.vue
@@ -6,7 +6,7 @@ defineProps({
 </script>
 
 <template>
-  <div class="bg-gray-900 text-white p-4 h-26 rounded text-right space-y-2 mb-4">
+  <div class="bg-gray-800 text-white p-4 h-26 rounded text-right space-y-2 mb-4">
     <div class="text-lg text-gray-400 truncate">{{ expression || '0' }}</div>
     <div class="text-3xl font-mono">{{ result || '' }}</div>
   </div>
